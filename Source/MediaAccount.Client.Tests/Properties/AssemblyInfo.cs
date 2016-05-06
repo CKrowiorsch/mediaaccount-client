008c9e60d57bd258091e27c9e5605555fce1e9d0
Christian Krowiorsch <christian.krowiorsch@googlemail.com>
2016-05-06 14:18:21 +0200

    intial

---
+++ Source/MediaAccount.Client.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("MediaAccount.Client.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MediaAccount.Client.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("f6827130-e40a-4d0e-ac66-d82c94e9902b")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ default.ps1
@@ -0,0 +1,30 @@
+Framework('4.5.1')
+$erroractionpreference = "Stop"
+
+properties {
+  $location = (get-location);
+  $outdir = (join-path $location "Build");
+  $artifactsdir = (join-path $outdir "Artifacts");
+  $bindir = (join-path $outdir "Bin");
+}
+
+task default -depends help
+
+
+task help {
+	write-host "Buildscript for MediaAccount client"
+	write-host "Tasks: rebuild"
+}
+
+task clean -depends NugetRestore {[void](rmdir -force -recurse $outdir -ea SilentlyContinue)
+}
+
+task nuget-restore {
+  exec { .nuget\nuget.exe restore }
+}
+
+task rebuild -depends clean,nuget-restore {
+  $solution = get-location;
+  exec { msbuild /nologo /v:minimal /t:rebuild /p:"Configuration=Release;OutputPath=$bindir/MediaAccount.Client/;SolutionDir=$solution/" "Source/MediaAccount.Client/MediaAccount.Client.csproj" }
+}
+
