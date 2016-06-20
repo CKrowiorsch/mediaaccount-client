05f79e65bb51ec7dbe97966847958137b1656c4f
Christian Krowiorsch <christian.krowiorsch@googlemail.com>
2016-06-20 17:45:16 +0200

    update psake.cmd script

--- psake.cmd
+++ psake.cmd
@@ -1,10 +1,3 @@
-@rem ==========================================================================
-@rem =                                                                        =
-@rem =            Autodeploy Psake script from SW                             =
-@rem =            v1.0                                                        =
-@rem =                                                                        =
-@rem ==========================================================================
-
 @ECHO OFF
 SET BASEPATH=%~dp0%
 SET NUGETPATH=%BASEPATH%.nuget\
@@ -12,21 +5,19 @@ SET NUGET=%NUGETPATH%nuget.exe
 
 rem check for nuget
 IF NOT EXIST %NUGET% (
-	ECHO .nuget\nuget.exe dose not exists. Please activate nuget package restore.
+	ECHO .nuget\nuget.exe does not exists. Please activate nuget package restore.
 	goto END
 )
 
 rem remove installed version to get the latest
-FOR /D %%p IN ("%BASEPATH%Packages\LandauMedia.PSake.Extended*") DO rmdir "%%p" /s /q >nul 2>nul
+FOR /D %%p IN ("%BASEPATH%Packages\PSake*") DO rmdir "%%p" /s /q >nul 2>nul
 
-rem ensure psake.extend is installed
-%NUGET% install -OutputDirectory Packages LandauMedia.PSake.Extended >nul
+rem ensure psake is installed
+%NUGET% install -OutputDirectory Packages PSake >nul
 
-rem get psake path from existing folder with version number
-for /f "tokens=*" %%F in ('dir /b /a:d "%BASEPATH%Packages\LandauMedia.PSake.Extended*"') do call set PSAKEPATH=%BASEPATH%Packages\%%F\
+REM Find psake with version
+for /f "tokens=*" %%F in ('dir /b /a:d "%BASEPATH%Packages\PSake*"') do call set PSAKEPATH=%BASEPATH%Packages\%%F\
 
-SET PSAKE=%PSAKEPATH%psake.cmd
-%PSAKE% -nologo %*
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%PSAKEPATH%\tools\psake.ps1' %*; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }"
 
-:END 
-
+:END
