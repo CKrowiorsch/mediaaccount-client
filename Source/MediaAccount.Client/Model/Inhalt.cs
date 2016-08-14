39390c20eeb7fe5f79daac0390cf616092ed81be
Christian Krowiorsch <christian.krowiorsch@googlemail.com>
2016-08-14 12:43:50 +0200

    Dokumentation für textinhalt

--- Source/MediaAccount.Client/Model/Inhalt.cs
+++ Source/MediaAccount.Client/Model/Inhalt.cs
@@ -1,13 +1,26 @@
 ﻿namespace Krowiorsch.MediaAccount.Model
 {
+    /// <summary>Gibt den textuellen Inhalt des Artikels an</summary>
     public class Inhalt
     {
         /// <summary>Headline des Artikels </summary>
         public string Headline { get; set; }
+
+        /// <summary>Subheadline des Artikels </summary>
         public string Subheadline { get; set; }
-        public object Abstract { get; set; }
+
+        /// <summary>Kurzfassung des Artikels </summary>
+        /// <remarks>wird nur zur Verfügung gestellt, wenn es extra bestellt wird.</remarks>
+        public string Abstract { get; set; }
+
+        /// <summary>Volltext des Artikels</summary>
+        /// <remarks>wird nur zur Verfügung gestellt, wenn das Lizenzrecht es erlaubt</remarks>
         public string Text { get; set; }
+
+        /// <summary>Kurztext des Artikels (Snippet)</summary>
         public string Previewtext { get; set; }
+
+        /// <summary>Autor des Artikels</summary>
         public string Autor { get; set; }
     }
 }
