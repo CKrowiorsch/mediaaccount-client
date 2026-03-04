d7c8982c2f03ec7553799aeb2c6b4be2647608af
Christian Krowiorsch <christian.krowiorsch@googlemail.com>
2026-03-04 18:20:09 +0100

    Erzwingt Nicht-Null-Initialisierung für Medienspiegel-Eigenschaften
    - Setzt `Id`, `Status` und `Kategorie` auf `null!` zur Sicherstellung von Nicht-Null-Werten
    - Verhindert potentielle Null-Referenzen

--- Source/MediaAccount.Client/Model/V2/Medienspiegel.cs
+++ Source/MediaAccount.Client/Model/V2/Medienspiegel.cs
@@ -5,11 +5,11 @@ namespace Krowiorsch.MediaAccount.Model.V2;
 public class Medienspiegel
 {
     [Description("Die Id des Medienspiegels.")]
-    public string Id { get; set; }
+    public string Id { get; set; } = null!;
 
     [Description("Gibt den Status der Meldung im Medienspiegel an.")]
-    public string Status { get; set; }
+    public string Status { get; set; } = null!;
 
     [Description("Kategorie in der die Meldung im Medienspiegel zugeordnet ist.")]
-    public string Kategorie { get; set; }
+    public string Kategorie { get; set; } = null!;
 }
